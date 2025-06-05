baf3a7b647f384bcdabc2f4b74b23cc64f787b30
yura76 <yriyvorobey@gmail.com>
2025-06-05 02:04:23 +0000

    ЛР4:Адмінка категорій

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Blog\Admin\CategoryController;
 use App\Http\Controllers\Blog\PostController;
 use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
@@ -22,4 +23,17 @@ Route::resource('rest', RestTestController::class)->names('restTest');
 
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
+});
+
+//Адмінка
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
 });
